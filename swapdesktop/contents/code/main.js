eb6081e21c1d1f714394c26b5f3529215b60a680
Daniel Cox <danielcox16@hotmail.com>
2024-06-11 16:27:59 +0200

    Start Plasma 6 update. Update Add and Remove functions. Update README and metadata.

--- swapdesktop/contents/code/main.js
+++ swapdesktop/contents/code/main.js
@@ -1,5 +1,5 @@
 /*
-*   Copyright (c) 2019 Daniël Cox <danielcox16@gmail.com>
+*   Copyright (c) 2024 Daniël Cox <danielcox16@gmail.com>
 *
 *   This program is free software; you can redistribute it and/or modify
 *   it under the terms of the GNU Lesser General Public License as published by
