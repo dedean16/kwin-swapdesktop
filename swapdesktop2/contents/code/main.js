eb8b26c0fe32e68e3809097ea602548e6ddee940
Daniel Cox <danielcox16@hotmail.com>
2024-06-12 12:23:58 +0200

    Add Remove before, Add swap (without creating new desktop)

--- swapdesktop2/contents/code/main.js
+++ swapdesktop2/contents/code/main.js
@@ -12,19 +12,47 @@
 *   GNU General Public License for more details.
 */
 
-function swap_desktops(n) {
-    // Swap desktops
-    print('To be implemented');
-}
-
 function get_current_desktop_index() {
+    // Return index of current desktop
     var current_index = workspace.desktops.indexOf(workspace.currentDesktop);
     return current_index;
 }
 
+
+function add_desktop_abs(index) {
+    // Create a new desktop at index
+    workspace.createDesktop(index, "New Desktop");
+}
+
+
 function add_desktop(relative_index) {
-    // Create a new virtual desktop at the end of the current set of desktops
-    workspace.createDesktop(get_current_desktop_index() + relative_index, "New Desktop");
+    // Create a new virtual desktop relative to current index
+    add_desktop_abs(get_current_desktop_index() + relative_index);
+}
+
+
+function swap_desktops_abs(index1, index2) {
+    // Swaps to desktops at specified indices
+    // Doesn't check if desktops at those indices exist
+    desktop1 = workspace.desktops[index1];
+    workspace.desktops[index1] = workspace.desktops[index2];
+    workspace.desktops[index2] = desktop1;
+}
+
+
+function swap_desktops(relative_index) {
+    // Swap desktops
+    current_index = get_current_desktop_index();
+    new_index = current_index + relative_index;
+
+    if (new_index < 0) {
+        print('to be implemented');
+    } else if (new_index >= workspace.desktops.length) {
+        add_desktop_abs(workspace.desktops.length);
+        print('to be implemented');
+    } else {
+        swap_desktops_abs(current_index, new_index);
+    }
 }
 
 
@@ -50,13 +78,18 @@ if (registerShortcut) {                                         /* Check if the 
                      "Meta+Shift+Alt+Down",
                      function() {swap_desktop(workspace.desktopGridWidth);});
     
-    registerShortcut("Add Desktop",                             /* Register shortcut for 'add desktop' */
-                     "swapdesktop2: Add new desktop between previous and current one (temp)",
+    registerShortcut("Add Desktop After Current",                             /* Register shortcut for 'add desktop' */
+                     "swapdesktop2: Add new desktop between current and next (temp)",
                      "Meta+Alt++",
                      function() {add_desktop(1);});
+    
+    registerShortcut("Add Desktop Before Current",                             /* Register shortcut for 'add desktop' */
+                     "swapdesktop2: Add new desktop between previous and current (temp)",
+                     "Meta+Alt+=",
+                     function() {add_desktop(0);});
 
-    registerShortcut("Remove Desktop",                          /* Register shortcut for 'insert desktop' */
+    registerShortcut("Remove Desktop",                          /* Register shortcut for 'remove desktop' */
                      "swapdesktop2: Remove current desktop and move its windows to the next (temp)",
-                     "Meta+Alt+_",
+                     "Meta+Alt+-",
                      function() {workspace.removeDesktop(workspace.currentDesktop);});
 }
