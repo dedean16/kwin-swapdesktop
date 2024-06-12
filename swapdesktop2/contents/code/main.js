e6dd82826cce954d0a197d644b195900ab8938dd
Daniel Cox <danielcox16@hotmail.com>
2024-06-12 16:48:16 +0200

    Control where to move windows when desktop is deleted

--- swapdesktop2/contents/code/main.js
+++ swapdesktop2/contents/code/main.js
@@ -12,6 +12,7 @@
 *   GNU General Public License for more details.
 */
 
+
 function get_current_desktop_index() {
     // Return index of current desktop
     var current_index = workspace.desktops.indexOf(workspace.currentDesktop);
@@ -31,6 +32,36 @@ function add_desktop(relative_index) {
 }
 
 
+function remove_current_desktop(relative_index) {
+    // Remove the current desktop and move its windows to the given relative index, and focus that desktop
+
+    // Compute new desktop index
+    var new_index = get_current_desktop_index() + relative_index;
+    if (new_index < 0) {
+        new_index = 0
+    } else if (new_index >= workspace.desktops.length) {
+        new_index = workspace.desktops.length - 1;
+    }
+
+    // Initialize
+    var all_desktops = workspace.desktops;
+    var all_windows = workspace.windowList();
+    var current_desktop_windows = [];
+    var destination_desktop = all_desktops[new_index];
+
+    // Make lists of windows on current desktop
+    for (var w = 0; w<all_windows.length; w++) {
+       var window = all_windows[w];
+        if (window.desktops[0] == workspace.currentDesktop) {
+            window.desktops[0] = destination_desktop;
+        }
+    }
+
+    // Remove the desktop
+    workspace.removeDesktop(workspace.currentDesktop);
+}
+
+
 function swap_desktop_abs(index1, index2) {
     // Checks all windows and swaps desktops at given indices (absolute)
     // Note: Doesn't check if desktops at those indices exist
@@ -45,7 +76,7 @@ function swap_desktop_abs(index1, index2) {
 
     // Make lists of windows on desktop1 and desktop2
     for (var w = 0; w<all_windows.length; w++) {
-        window = all_windows[w];
+        var window = all_windows[w];
         if (window.desktops[0] == desktop1) {
             desktop1_windows.push(window);
         } else if (window.desktops[0] == desktop2) {
@@ -60,10 +91,11 @@ function swap_desktop_abs(index1, index2) {
 
 
 function swap_desktop(relative_index) {
-    // Swap desktops
+    // Add desktop if required, swap desktops, focus swapped desktop
     current_index = get_current_desktop_index();
     new_index = current_index + relative_index;
 
+    // Add new desktop to make swap possible, if required
     if (new_index < 0) {
         add_desktop_abs(0);
         current_index++;
@@ -71,44 +103,52 @@ function swap_desktop(relative_index) {
     } else if (new_index == workspace.desktops.length) {
         add_desktop_abs(workspace.desktops.length);
     }
+
     swap_desktop_abs(current_index, new_index);
+
+    workspace.currentDesktop = workspace.desktops[new_index];
 }
 
 
-if (registerShortcut) {                                         /* Check if the register function actually exists */
+if (registerShortcut) {    /* Check if the register function actually exists */
     
-    registerShortcut("Swap with Next Desktop (temp4)",                  /* Register shortcut for 'swap with next desktop' */
-                     "swapdesktop2: Swap current desktop with next one (temp4)",
+    registerShortcut("Swap windows with next desktop (temp5)",
+                     "swapdesktop2: Swap current desktop with next one (temp5)",
                      "Meta+Shift+Alt+Right",
                      function() {swap_desktop(1);});
-    
-    registerShortcut("Swap with Previous Desktop (temp4)",              /* Register shortcut for 'swap with previous desktop' */
-                     "swapdesktop2: Swap current desktop with previous one (temp4)",
+
+    registerShortcut("Swap windows with previous desktop (temp5)",
+                     "swapdesktop2: Swap current desktop with previous one (temp5)",
                      "Meta+Shift+Alt+Left",
                      function() {swap_desktop(-1);});
 
-    registerShortcut("Swap with Above Desktop (temp4)",                 /* Register shortcut for 'swap with above desktop' */
-                     "swapdesktop2: Swap current desktop with one above (temp4)",
+    registerShortcut("Swap with above desktop (temp5)",
+                     "swapdesktop2: Swap current desktop with one above (temp5)",
                      "Meta+Shift+Alt+Up",
                      function() {swap_desktop(-workspace.desktopGridWidth);});
-    
-    registerShortcut("Swap with Below Desktop (temp4)",                 /* Register shortcut for 'swap with below desktop' */
-                     "swapdesktop2: Swap current desktop with one below (temp4)",
+
+    registerShortcut("Swap with below desktop (temp5)",
+                     "swapdesktop2: Swap current desktop with one below (temp5)",
                      "Meta+Shift+Alt+Down",
                      function() {swap_desktop(workspace.desktopGridWidth);});
-    
-    registerShortcut("Add Desktop After Current (temp4)",                             /* Register shortcut for 'add desktop' */
-                     "swapdesktop2: Add new desktop between current and next (temp4)",
+
+    registerShortcut("Add desktop after current (temp5)",
+                     "swapdesktop2: Add new desktop between current and next (temp5)",
                      "Meta+Alt++",
                      function() {add_desktop(1);});
-    
-    registerShortcut("Add Desktop Before Current (temp4)",                             /* Register shortcut for 'add desktop' */
-                     "swapdesktop2: Add new desktop between previous and current (temp4)",
+
+    registerShortcut("Add desktop before current (temp5)",
+                     "swapdesktop2: Add new desktop between previous and current (temp5)",
                      "Meta+Alt+=",
                      function() {add_desktop(0);});
 
-    registerShortcut("Remove Desktop (temp4)",                          /* Register shortcut for 'remove desktop' */
-                     "swapdesktop2: Remove current desktop and move its windows to the next (temp4)",
+    registerShortcut("Remove desktop, move windows to previous (temp5)",
+                     "swapdesktop2: Remove current desktop and move its windows to the previous (temp5)",
+                     "Meta+Alt+_",
+                     function() {remove_current_desktop(1);});
+
+    registerShortcut("Remove desktop, move windows to next (temp5)",
+                     "swapdesktop2: Remove current desktop and move its windows to the next (temp5)",
                      "Meta+Alt+-",
-                     function() {workspace.removeDesktop(workspace.currentDesktop);});
+                     function() {remove_current_desktop(-1);});
 }
